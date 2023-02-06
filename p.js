1b0b39819c75ef457da98689381e3596d1fdeb1e
sajim-khan <shossain1427@gmail.com>
2023-02-06 23:50:09 +0600

    added

--- p.js
+++ p.js
@@ -3,3 +3,8 @@ for( let i = 0; i<=3;i++){
 sum = sum + i; 
 }
 console.log(sum);
+
+const nayoks=['Bangla Bhai','English Bhai','Korean Bhai','Turkish Bhai'] 
+for (const nayok of nayoks) {
+  console.log(nayok);
+}
