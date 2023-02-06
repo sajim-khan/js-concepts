033a312dfbe31ef03c9e88fae3fa7505c2b2bd52
sajim-khan <shossain1427@gmail.com>
2023-02-06 23:48:26 +0600

    added

--- milestokm.js
+++ milestokm.js
@@ -3,4 +3,31 @@ function milesToKm(miles){
     return km;
 }
 const kilometer = milesToKm(2)
-console.log('kilometer :',kilometer);
+//console.log('kilometer :',kilometer);
+
+function print(a, b, c){
+    return a+2;
+    return a*b;
+    return b*c+a;
+  }
+  //console.log(print(1,2,3));
+
+  const number = -78; 
+  const answer = Math.abs(number); 
+  //console.log(answer);
+
+  var a=2; 
+  var b=3; 
+  if(a < b){
+      console.log("Hello");
+  }
+
+  function add(a, b){
+    return a + b;
+  }
+  //console.log(add("adam" + "eve"))
+
+  const nayoks=['Bangla Bhai','English Bhai','Korean Bhai','Turkish Bhai'] 
+  for (const nayok of nayoks) {
+    console.log(nayok);
+  }
