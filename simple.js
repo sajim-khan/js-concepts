eacc8e8463fecb9d47d6d5cb8f0d41a3b8adaec4
sajim-khan <shossain1427@gmail.com>
2023-02-07 23:22:02 +0600

    added

---
+++ simple.js
@@ -0,0 +1,15 @@
+//increment
+let sum = 0;
+for (let i = 1; i <= 5; i++) {
+    sum = sum + i;
+}
+console.log(sum)
+
+
+//decrement
+let add = 0;
+for (let i = 5; i >=1; i--) {
+    add = add + i;
+}
+console.log(add)
+
