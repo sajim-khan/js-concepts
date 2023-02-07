8a97ce591be7ea615db5ba77e6bcd486de61f0a7
sajim-khan <shossain1427@gmail.com>
2023-02-07 23:45:50 +0600

    added factorial

--- simple.js
+++ simple.js
@@ -1,15 +1,24 @@
-//increment
-let sum = 0;
-for (let i = 1; i <= 5; i++) {
-    sum = sum + i;
+// //increment
+// let sum = 0;
+// for (let i = 1; i <= 5; i++) {
+//     sum = sum + i;
+// }
+// console.log(sum)
+
+
+// //decrement
+// let add = 0;
+// for (let i = 5; i >=1; i--) {
+//     add = add + i;
+// }
+// console.log(add)
+
+
+function sum(i){
+    if(i == 5){
+        return 5;
+    }
+    return i + sum(i + 1);
 }
-console.log(sum)
-
-
-//decrement
-let add = 0;
-for (let i = 5; i >=1; i--) {
-    add = add + i;
-}
-console.log(add)
-
+const result = sum(1)
+console.log(result)
