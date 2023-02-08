bc7f44cf4238d86637a124401a11251306e3a335
sajim-khan <shossain1427@gmail.com>
2023-02-08 16:04:45 +0600

    added

---
+++ object.js
@@ -0,0 +1,12 @@
+const student = {
+    name : 'Sajib',
+    id : 11223,
+    age : 28,
+    friends : ['sajim', 'shuvo', 'jihad', 'hasib'],
+    car : {
+        brand : 'Toyota',
+        model : '2018',
+        price : '$50000',
+    }
+}
+console.log(student);
