40165a3e70d793593eabef9474bc0b7d5382aea4
sajim-khan <shossain1427@gmail.com>
2023-02-08 16:19:27 +0600

    added various types of object

--- object.js
+++ object.js
@@ -1,12 +1,25 @@
-const student = {
-    name : 'Sajib',
+//object -> object -> object 
+//object -> Array -> object 
+const nayok = {
+    name : 'Sakib Khan',
     id : 11223,
     age : 28,
     friends : ['sajim', 'shuvo', 'jihad', 'hasib'],
+    movies : [{name : 'no 1 sakib khan', year : 2016}, {name : 'Rongbaz', year : 2018}],
     car : {
         brand : 'Toyota',
         model : '2018',
         price : '$50000',
+        manufacturer : {
+            name : 'Toyota',
+            ceo : 'elon mask',
+            country : 'US'
+        }
+    },
+    act :function(){
+        console.log("Acting like sakib khan");
     }
 }
-console.log(student);
+console.log(nayok);
+console.log(nayok.act);
+nayok.act()
